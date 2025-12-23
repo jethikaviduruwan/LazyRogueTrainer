94d59e5e50154eb3c0028c1fc346ccbc31e153c1
Jethika Wanniarachchi <49968486+jethikaviduruwan@users.noreply.github.com>
2025-12-23 15:26:28 +0530

    Update files

--- src/Test.cpp
+++
@@ -1,8 +0,0 @@
-#include "Test.h"
-#include <stdio.h>
-
-namespace LazyRogueTrainer {
-	void Print() {
-		printf("Welcome to LazyRogueTrainer Engine");
-	}
-}
--- src/Test.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace LazyRogueTrainer {
-	_declspec(dllexport) void Print();
-}
